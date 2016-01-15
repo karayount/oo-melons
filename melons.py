57d60478e774616a5bf26b4859ded7d3d94d080f
Hackbright Student <info@hackbrightacademy.com>
2016-01-14 18:04:57 -0800

    Initial commit utilizing inheritance concepts

---
+++ melons.py
@@ -0,0 +1,91 @@
+"""This file should have our order classes in it."""
+
+import random
+import datetime
+
+class AbstractMelonOrder(object):
+    """A melon order abstract with attributes, methods common to all orders."""
+
+    def __init__(self, species, qty):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = None
+
+
+    def get_base_price(self):
+        """Randomly chooses base price from 5 to 9"""
+
+        base_price = random.randint(5,9)
+        print datetime.datetime.now().time()
+        # print datetime.time.hour
+        # print datetime.date.weekday()
+        if datetime.time.hour in range(8,20) and datetime.date.weekday() in range(0,5): 
+            base_price += 4
+            print base_price
+        return base_price
+
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = self.get_base_price()
+        total = (1 + self.tax) * self.qty * base_price
+        if self.species == "Christmas":
+            total *= 1.5
+        if self.qty < 10 and self.order_type == "international":
+            total += 3
+        return total
+
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes"""
+
+        super(DomesticMelonOrder, self).__init__(species, qty)
+        self.order_type = "domestic"
+        self.tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes"""
+
+        super(InternationalMelonOrder, self).__init__(species, qty)
+        self.country_code = country_code
+        self.order_type = "international"
+        self.tax = 0.17
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A government melon order."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+        super(GovernmentMelonOrder, self).__init__(species, qty)
+        self.tax = 0
+        self.order_type = "government" 
+        self.passed_inspection = False
+
+
+    def inspect_melons(self, passed):
+        """Takes in boolean of ispection passed, returns boolen for inspection passed."""
+
+        if passed == True:
+            self.passed_inspection = True
